7639f4989c7ec4330ff38263ac3212ae4955a8f2
Boardens <31590269+boardens@users.noreply.github.com>
2020-06-27 04:45:21 +0200

    Add files via upload

---
+++ simple_pastebin.py
@@ -0,0 +1,38 @@
+import mechanize
+
+def login(username, password):
+	br.open("https://pastebin.com/login/")
+	formcount = 0
+	for frm in br.forms():  
+		if str(frm.attrs["method"]) == "post":
+			break
+		formcount += 1
+	br.select_form(nr=formcount)
+	br["user_name"] = username
+	br["user_password"] = password
+	br.submit()
+	if br.geturl() == "https://pastebin.com/u/"+username:
+		return True
+	for i in range(1, 7):
+		if br.geturl() == "https://pastebin.com/login.php?e="+str(i):
+			return False
+	else:
+		return False
+
+def logout():
+	try:
+		br.open("https://pastebin.com/logout")
+		return True
+	except Exception:
+		return False
+
+def paste(content, name="", expire="N", exposure="0", formatting="1"):
+	br.open("https://pastebin.com/")
+	br.select_form("myform")
+	br["paste_code"] = content
+	br["paste_format"] = [formatting]
+	br["paste_expire_date"] = [expire]
+	br["paste_private"] = [exposure]
+	br["paste_name"] = name
+	br.submit()
+	return br.geturl()
