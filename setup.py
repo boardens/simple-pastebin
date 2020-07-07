bf9a7cde5f365572f74b0df495d4a026277def27
Boardens <31590269+boardens@users.noreply.github.com>
2020-07-07 10:03:55 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import simple_pastebin.__init__ as init
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=init.module_name,
+    version=init.__version__,
+    description="Simple pastebin unofficial wrapper based on web automations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/boardens/simple-pastebin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ simple_pastebin/__init__.py
@@ -0,0 +1,4 @@
+from .main import *
+
+module_name = "simple_pastebin"
+__version__ = "1.0.0"
